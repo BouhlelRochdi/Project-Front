7236a180bb1f1400c9014fd4ed51483f1aa74785
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-05-11 16:27:52 +0100

    create service for Company

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,13 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  constructor() { }
+  constructor( private http : HttpClientModule) { }
 
   addCompany(data){
+    // this.http.post(data);
     console.log(data);
   }
 }
