d2b78a0506ec5454f9495cdc5bc7311f58a976d8
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-05-22 10:55:33 +0100

    add any to parameter type

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
@@ -10,11 +9,11 @@ export class CompanyService {
 
   constructor(private http: HttpClient) { }
 
-  addCompany(data): Observable<any> {
-    return this.http.post('http://localhost:27017/api/register', data);
+  register(data: any): Observable<any> {
+    return this.http.post('http://localhost:3000/api/register', data);
   }
 
-  login(data): Observable<any> {
-    return this.http.get('http://localhost:27017/api/login', data);
+  login(data: any): Observable<any> {
+    return this.http.post('http://localhost:3000/api/login', data);
   }
 }
