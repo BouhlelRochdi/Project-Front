4a8f656b1647b506a22807820ec79fbf258ec2f2
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-06-09 12:00:10 +0100

    firstCommit

--- src/app/views/events/events.module.ts
+++ src/app/views/events/events.module.ts
@@ -4,16 +4,27 @@ import { CommonModule } from '@angular/common';
 import { EventsRoutingModule } from './events-routing.module';
 import { EventsComponent } from './events.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { SelectModule } from 'ng-select';
+// import { DataTableComponent } from '../tables/datatable/datatable.component';
+// import { DataFilterPipe } from '../tables/datatable/datafilterpipe';
 
 
 @NgModule({
   declarations: [
-    EventsComponent
+    EventsComponent,
+    // DataTableComponent,
+    // DataFilterPipe
   ],
   imports: [
     CommonModule,
     EventsRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SelectModule,
+    // DatatableRoutingModule,
+    CommonModule,
+    // DataTableModule,
+    // FormsModule,
+    // HttpClientModule
   ]
 })
 export class EventsModule { }
