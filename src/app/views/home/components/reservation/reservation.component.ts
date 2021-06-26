c55cdca7488cffb90dfa57256dc772de5c7aa101
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-06-26 09:33:51 +0100

    Add multi reservation and fix errors

--- src/app/views/home/components/reservation/reservation.component.ts
+++ src/app/views/home/components/reservation/reservation.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ToasterService } from 'angular2-toaster';
+import { ReservationService } from '../../../../services/reservation.service';
 
 @Component({
   selector: 'app-reservation',
@@ -8,44 +10,77 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./reservation.component.css']
 })
 export class ReservationComponent implements OnInit {
-  reservationForm : FormGroup;
+  reservationForm: FormGroup;
   submited = false;
   id;
+  indexRow = null;
 
-  constructor(private activedRouter: ActivatedRoute) { }
+  constructor(private activedRouter: ActivatedRoute,
+    private reservationService : ReservationService,
+    private toaster : ToasterService,
+    private router : Router) { }
 
   ngOnInit(): void {
     this.id = this.activedRouter.snapshot.params.id;
     this.reservationForm = new FormGroup({
-      firstName: new FormControl('', Validators.required),
-      lastName: new FormControl('', Validators.required),
-      email: new FormControl('', [Validators.required, Validators.email]),
-      newReservation: new FormArray([])
+      user: new FormGroup({
+        fName: new FormControl('', Validators.required),
+        lName: new FormControl('', Validators.required),
+        email: new FormControl('', [Validators.required, Validators.email]),
+      }),
+      reservations: new FormArray([])
     });
-
-
   }
 
-  get getNewReservation():FormArray{
-    return this.reservationForm.controls.newReservationArray as FormArray;
+  
+  get userForm(): FormGroup {
+    return this.reservationForm.controls.user as FormGroup;
   }
 
-  addReservation(){
-    this.getNewReservation.push(new FormGroup({
-      fName: new FormControl('', Validators.required),
-      lName: new FormControl('', Validators.required),
-      otherEmail: new FormControl('', Validators.required)
-    }));
+  get getReservations(): FormArray {
+    return this.reservationForm.controls.reservations as FormArray;
   }
 
-  valider(){
+  addReservation() {
+    this.reservationService.createReservation(this.id, this.reservationForm.value).subscribe(res => {
+      this.getReservations.push(new FormGroup({
+        fName: new FormControl('', Validators.required),
+        lName: new FormControl('', Validators.required),
+        email: new FormControl('', Validators.required)
+      }));
+    }, err =>{
+      this.toaster.pop('error', 'Stock is empty', err.error.message);
+    })
+  }
+
+  // remove reservation from formArray 
+  removeReservation(i: number){
+    this.getReservations.removeAt(i);
+  }
+
+  valider() {
     this.submited = true;
-    if(this.reservationForm.invalid){
-      console.log('Invalid Form');
+    if (this.reservationForm.invalid) {
+      this.toaster.pop('warning', 'Invalid form .. pleas check more')
       return;
     }
-    console.log('Form Valide');
-    
+    else{
+      this.reservationService.createReservation(this.id, this.reservationForm.value).subscribe(res => {
+        this.toaster.pop('success', 'please check your email','Reservation complete successfully')
+      },
+      err => {
+        this.toaster.pop('error', 'Something goes wrong!', err.error.message)
+      })
+      this.reservationForm.reset();
+      this.submited= false;
+      this.router.navigateByUrl('/home');
+    }
+
+  }
+
+  removeAllRow()
+  {
+    this.getReservations.clear();
   }
 
 }
