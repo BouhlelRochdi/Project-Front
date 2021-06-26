9dda3060bef29db45da2fc9ef353f6a8df6e5a2f
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-06-26 09:35:32 +0100

    Create reservation service

--- src/app/services/reservation.service.ts
+++ src/app/services/reservation.service.ts
@@ -1,9 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ReservationService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  createReservation(id, data): Observable <any>{
+    return this.http.post(`http://localhost:3000/api//reservation/${id}`, data)
+  }
 }
