64cfcc4a33b0f432828de0f091920c79d5e3ff62
Rochdi Bouhlel <rochdi.bouhlel@hotmail.fr>
2021-06-29 18:33:04 +0100

    Create navbar home

--- src/app/views/home/home.module.ts
+++ src/app/views/home/home.module.ts
@@ -6,13 +6,15 @@ import { HomeComponent } from './home.component';
 import { EventDetailComponent } from './components/event-detail/event-detail.component';
 import { ReservationComponent } from './components/reservation/reservation.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
     EventDetailComponent,
-    ReservationComponent
+    ReservationComponent,
+    NavbarComponent
   ],
   imports: [
     CommonModule,
